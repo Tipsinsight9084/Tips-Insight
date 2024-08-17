cfc54655955cf4d8736fb83cfe583a46708324db
Tipsinsight9084 <acepredictionlowrates@gmail.com>
2024-08-17 23:54:12 +0530

    rounded

--- src/screenshot.jsx
+++ src/screenshot.jsx
@@ -7,7 +7,7 @@ export default function Images(props){
         <>
         <div className="px-2 mb-8 flex flex-column justify-center bg-gradient-to-r from-customRed to-customBrown rounded-2xl mx-4">
   <div className="w-full"> 
-    <img src={ss} className="w-full my-4" alt="Image description" />
+    <img src={ss} className="w-full my-4 rounded-2xl" alt="Image description" />
   </div>
 </div>
 
